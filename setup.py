b3679f56e27ec57d30d8e4eb65456fe7cd5ffd44
kevindkeogh <kevin.d.keogh@gmail.com>
2015-08-23 21:59:34 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'qlibpy',
+    version = '0.1',
+    description = 'QuantLib Python wrapper',
+    author = 'Kevin Keogh',
+    author_email = 'kevin[dot]d[dot]keogh[at]gmail[dot]com',
+    url = 'https://github.com/kevindkeogh/qlibpy',
+    license = 'MIT',
+    packages = find_packages()
+    )
