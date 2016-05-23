bf63f4909055a21025a8d62d7e8ad8ec95d66a5c
Kevin Keogh <kevin.d.keogh@gmail.com>
2016-05-22 23:26:11 -0400

    more work on schedule

--- helpers/schedule.py
+++ helpers/schedule.py
@@ -8,6 +8,26 @@ import numpy as np
 
 class Schedule:
     '''
+    Class to generate and hold interest rate swap accrual schedules. The class
+    takes the effective and maturity dates, and a few optional parameters to
+    enable exact schedule creation.
+    Arguments:
+        effective(datetime)     Effective date of schedule
+        maturity(datetime)      Maturity of schedule
+        backward(bool)          True/False to generate dates from end or start
+                                [default:True][optional]
+        second(datetime)        First date after effective date [optional]
+        penultimate(datetime)   Last date before maturity date [optional]
+        weeks(int)              Number of weeks for each reset [optional]
+        months(int)             Number of months for each period [optional]
+
+    Returns
+        Schedule                Object that holds the accrual periods
+                                2d numpy array (1st dimension is accrual
+                                start dates, 2nd dimension is accrual end dates,
+                                3rd dimension is payment dates, 4th dimension is
+                                fixing dates)
+    
     '''
     def __init__(self, effective, maturity, backward=True, second=None,
                  penultimate=None, weeks=None, months=None):
@@ -24,6 +44,8 @@ class Schedule:
 
     def _check_inputs(self):
         '''
+        Checking function for inputs to Schedule. Trys to catch common errors
+        that will cause a failure to create a sensible schedule.
         '''
         if self.maturity == self.penultimate:
             raise Exception('Maturity and penultimate dates cannot be equal')
@@ -37,7 +59,10 @@ class Schedule:
 
     def _gen_period_ends(self):
         '''
-        Generate 
+        Generate the period end dates. Uses the dateutil module to generate
+        a repeating rule to generate a list of dates.
+        Returns:
+            period_ends (np array)  Numpy 1d array of period end datetimes.
         '''
         if self.months is not None:
             period_type = rrule.MONTHLY
@@ -45,14 +70,14 @@ class Schedule:
         elif weeks is not None:
             period_type = rrule.WEEKLY
             period_length = self.weeks
+        if self.second is not None:
+            start = second
+            period_ends = [effective]
+        else:
+            start = effective
+            period_ends = []
         if self.backward:
-            if self.penultimate is not None:
-                if self.second is not None:
-                    start = second
-                    period_ends = [effective]
-                else:
-                    start = effective
-                    period_ends = []
+            if self.penultimate is not None: # Backward from penultimate
                 period_ends += list(rrule.rrule(period_type, 
                                                 interval=period_length, 
                                                 bymonthday=(self.penultimate.day, -1), #these 2
@@ -64,9 +89,25 @@ class Schedule:
                                     'occur at the right interval?')
                 period_ends = np.append(np.array(period_ends), np.array([maturity]))
                 return period_ends
+            else: # TODO: Backward from maturity
+                pass
+        else: 
+            period_ends += list(rrule.rrule(period_type,
+                                            interval=period_length,
+                                            bymonthday=(self.maturity.day, -1),
+                                            bysetpos=1,
+                                            dtstart=start,
+                                            until=maturity))[1:]
+            period_ends = np.array(period_ends)
+            if period_ends[-1] != maturity:
+               period_ends = np.append(period_ends, np.array([maturity]))
+            return period_ends
 
-effective = datetime.datetime(2011, 11, 11)
-maturity = datetime.datetime(2012, 11, 11)
-penultimate = datetime.datetime(2012, 8, 11)
-sched = Schedule(effective, maturity, penultimate=penultimate, months=3)
-print(sched.period_ends)
+
+
+if __name__ == '__main__':
+    effective = datetime.datetime(2011, 11, 11)
+    maturity = datetime.datetime(2012, 11, 11)
+    penultimate = datetime.datetime(2012, 8, 11)
+    sched = Schedule(effective, maturity, penultimate=penultimate, months=3)
+    print(sched.period_ends)
